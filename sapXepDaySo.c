44fc5eaeff9d047b1a081fd7e74cb3d1d558ef51
khoenguyen1 <53784085+khoenguyen1@users.noreply.github.com>
2019-08-06 23:00:44 +0900

    updateFormat

--- sapXepDaySo.c
+++ sapXepDaySo.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 
-int main(void){
-//sap xep day so
-    int mang[5] = {3, 23, 45, 19, 27};
-    int i = 0, j = 0; 
-    int tmp;
-    
-    for (i; i < 4; i ++) {
-        for (j = 0; j < 4; j++) {
-            if (mang[j] < mang[j + 1]) {
-                tmp = mang[j + 1];
-                mang[j + 1] =mang [j];
-                mang[j] = tmp;
-             }
-        }
-    }
-    printf ("arrange from big to small : %d, %d, %d, %d,%d", mang[0], mang[1], mang[2], mang[3], mang[4]);
+int sort(int mang[]);
+void display(int mang[]);
+int main(void) {
+  //sap xep day so
+  int mang[5] = {
+    3,
+    23,
+    45,
+    19,
+    27
+  };
+  sort(mang);
+  display(mang);
 }
 
+int sort(int mang[]) {
+  int i = 0, j = 0;
+  int tmp;
+
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < 4; j++) {
+      if (mang[j] < mang[j + 1]) {
+        tmp = mang[j + 1];
+        mang[j + 1] = mang[j];
+        mang[j] = tmp;
+      }
+    }
+  }
+}
+void display(int mang[]) {
+  printf("arrange from big to small : %d, %d, %d, %d,%d", mang[0], mang[1], mang[2], mang[3], mang[4]);
+}
